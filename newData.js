97a7a9b4cb3f0b92f904cd43e0d24a9cb7eb95ec
Zephaniah Ansah <144151400+Zansah@users.noreply.github.com>
2025-08-08 00:53:32 -0400

    Add files via upload

---
+++ newData.js
@@ -0,0 +1,56 @@
+const newsData = [
+    {
+        id: 1,
+        title: "Teaching Assistant for William & Mary's Phage Lab",
+        shortDescription: "Assisting in hands-on bacteriophage discovery under Prof. Margaret Saha.",
+        date: "August 27, 2024",
+        image: "photo/new-section/testTube.jpg",
+        fullContent: "Coming Soon"  
+    },
+    {
+        id: 2,
+            title: "Meet the New Captain of the Open Volleyball Team",
+            shortDescription: "Join us in the MAC Friday from 3 to 5PM",
+            date: "August 24, 2025",
+            "image": "photo/new-section/JustDoItHinita.jpg",
+            fullContent: "Here's the full article content for the Sonic Dino game..."
+    },
+    {
+        id: 3,
+        title: "Junior Mechanic at the Williamsburg Bike Co-op",
+        shortDescription: "Providing affordable, safe bikes and helmets to the community.",
+        date: "July 11, 2025",
+        image: "photo/new-section/roadBikeOrangeBackground.jpg",
+        fullContent: "Coming Soon."
+    },
+    {
+        id: 4,
+        title: "Presented “Champion or Cheater?: Where Worlds Collide in Competitive Game Play” at ERNC 2024",
+        shortDescription: "Explored the psychology of cheating in esports at the London conference.",
+        date: "October 31, 2024",
+        image: "photo/new-section/Whereworldcollide.webp",
+        fullContent: "Coming Soon."
+    },
+    {
+        id: 5,
+        title: "Independent Studies – Esports Pioneer at William & Mary",
+        shortDescription: "Collaborating on W&M Esports website improvements using Cascade.",
+        date: "September 15, 2024",
+        image: "photo/new-section/williamandmaryesports.jpeg",
+        fullContent: "Coming Soon."
+    },
+    {
+        id: 6,
+        title: "Wren Scholar Research on Novel Mycobacteriophages",
+        shortDescription: "SEA-PHAGES team presents findings at April 2024 symposium.",
+        date: "April 4, 2024",
+        image: "photo/new-section/seaphageslogo.png",
+        fullContent: "From September 2023 to April 2024, I participated in the Wren Scholar program at William & Mary, engaging in a year-long research experience focused on isolating and characterizing novel mycobacteriophages. Our team presented the poster 'A Tale of Two F1 Phages: Doc and Deb's Wild Adventure' at the SEA-PHAGES Symposium in April 2024. We also submitted the full annotation of DocMcStuffins and Deb65 Mycobacteriophages to GenBank and contributed to the paper 'Isolation and Characterization of Two Novel F1 Cluster Phages.'"
+    },
+    
+    
+    
+      
+
+
+];
